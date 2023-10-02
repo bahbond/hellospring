b81eb0559e4efe50617a00a6586c919d4bed046f
bahbond <bondopicks@gmail.com>
2023-10-02 13:09:04 -0500

    Hello Spring

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,98 +4,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
+    //handle request at path http://localhost:8080/hello
 
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
 
-    //lives at /hello/goodbye
+    //handle request at path //http://localhost:8080/goodbye
     @GetMapping("goodbye")
-
-    public String goodbye() {
-        return "GoodBye, Spring!";
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring!";
     }
 
-
-    //lives at hello/hello
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-
-    public String helloWithQueryParam(@RequestParam String name) {
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-
-    public String helloWithPathParam(@PathVariable String name) {
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
-
+    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action ='hello' method ='post' action = '/hello'>" +//submit a reqeust to /hello
-                "<input type='text' name='name'>" +
-                "<select name='language' id ='language-select'> <option value='english'>English</option>\n" +
-                "                <option value='french'>French</option>\n" +
-                "                <option value='spanish'>Spanish</option>\n" +
-                "                <option value='swahili'>Swahili</option>\n" +
-                "                <option value='german'>German</option>\n" +
-                "                </select>"
-                +
-
-                "<input type='submit' value='Greet me!'" +
+                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
-
-    }
-    @RequestMapping(value="hell0/form", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
-
-        return createMessage(name, language);
-
-        // For a bonus mission, students can change this response text to look nicer.
-        // This is subjective, but students should be modifying the HTML of the response string.
-    }
-
-    public static String createMessage(String n, String l) {
-        String greeting = "";
-
-        if (l.equals("english")) {
-            greeting = "Hello";
-        }
-        else if (l.equals("french")) {
-            greeting = "Bonjour";
-        }
-        else if (l.equals("spanish")) {
-            greeting = "Bonjourno";
-        }
-        else if (l.equals("swahili")) {
-            greeting = "Hola";
-        }
-        else if (l.equals("german")) {
-            greeting = "Hallo";
-        }
-
-        return greeting + " " + n;
     }
 
 }
-//    @RequestMapping(method = RequestMethod.POST)
-//        public static String createMessage (String name, String language) {
-//        if () {
-//            return "Hello" + name;
-//        } ;
-//        else{
-//            return "Try Again!";
-//        }
-//    }
-//   }
-
